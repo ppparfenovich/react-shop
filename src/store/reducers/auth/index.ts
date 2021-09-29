c0d1f03368c71fba22583b13da5d2bc62ae2f49b
Pavel Parfianovich <pavel_parfianovich@epam.com>
2021-09-29 16:56:27 +0300

    fix: eslint

---
+++ src/store/reducers/auth/index.ts
@@ -0,0 +1,24 @@
+import { User } from '../../../types/User'
+import { AuthAction, AuthActionEnum, AuthState } from './types'
+
+const initialState: AuthState = {
+  isAuth: false,
+  error: '',
+  isLoading: false,
+  user: {} as User,
+}
+
+export default function authReducer(state = initialState, action: AuthAction): AuthState {
+  switch (action.type) {
+    case AuthActionEnum.SET_AUTH:
+      return { ...state, isAuth: action.payload, isLoading: false }
+    case AuthActionEnum.SET_USER:
+      return { ...state, user: action.payload }
+    case AuthActionEnum.SET_ERROR:
+      return { ...state, error: action.payload, isLoading: false }
+    case AuthActionEnum.SET_IS_LOADING:
+      return { ...state, isLoading: action.payload }
+    default:
+      return state
+  }
+}
---
+++ src/store/reducers/auth/types.ts
@@ -0,0 +1,34 @@
+import { User } from '../../../types/User'
+
+export interface AuthState {
+  isAuth: boolean
+  user: User
+  isLoading: boolean
+  error: string
+}
+
+export enum AuthActionEnum {
+  SET_AUTH = 'SET_AUTH',
+  SET_ERROR = 'SET_ERROR',
+  SET_USER = 'SET_USER',
+  SET_IS_LOADING = 'SET_IS_LOADING',
+}
+
+export interface SetAuthAction {
+  type: AuthActionEnum.SET_AUTH
+  payload: boolean
+}
+export interface SetErrorAction {
+  type: AuthActionEnum.SET_ERROR
+  payload: string
+}
+export interface SetUserAction {
+  type: AuthActionEnum.SET_USER
+  payload: User
+}
+export interface SetIsLoadingAction {
+  type: AuthActionEnum.SET_IS_LOADING
+  payload: boolean
+}
+
+export type AuthAction = SetAuthAction | SetUserAction | SetErrorAction | SetIsLoadingAction
